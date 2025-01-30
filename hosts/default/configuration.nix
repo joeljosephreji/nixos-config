e7d62ac40fcd615077b7d99b1aaf01d8f11b108c
Joel Joseph Reji <joeljosephreji@gmail.com>
2025-01-30 13:54:33 +0530

    configuration files

---
+++ hosts/default/configuration.nix
@@ -0,0 +1,170 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
+
+  # Flakes and nix command
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      devices = [ "nodev" ];
+      efiSupport = true;
+      useOSProber = true;
+    };
+  };
+
+  # Latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  networking.nameservers = [
+    "1.1.1.1"
+    "1.0.0.1"
+  ];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_IN";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.kierkegaard = {
+    isNormalUser = true;
+    description = "kierkegaard";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  git
+  kitty
+  networkmanagerapplet
+  brightnessctl
+  playerctl
+  # TODO maybe remove the following packages from here later
+  stow
+  waybar
+  rofi-wayland
+  #  wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # For firefox
+  programs.firefox.enable = true;
+
+  # For electron apps to use wayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # Hyprland
+  programs.hyprland.enable = true;
+
+  # List services that you want to enable:
+
+  # Display Manager.
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    useUserPackages = true;
+    useGlobalPkgs = true;
+    users = {
+      "kierkegaard" = import ./home.nix;
+    };
+  };
+
+  fonts = {
+    enableDefaultPackages = true;
+    enableGhostscriptFonts = true;
+    packages = with pkgs; [
+      nerd-fonts.fira-code
+      nerd-fonts.fira-mono
+      fira-code
+      fira-sans
+      font-awesome
+      # fira-go # apparently building the fonts and not working
+      # fira-math # apparently building the fonts and not working
+    ];
+  };
+
+  services = {
+    supergfxd.enable = true;
+    asusd = {
+      enable = true;
+      enableUserService = true;
+    };
+  };
+
+}
---
+++ hosts/default/home.nix
@@ -0,0 +1,159 @@
+{ config, pkgs, ... }:
+
+# TODO figure out firefox
+# TODO figure out how to theme gtk (nwlook) and qt applications - use catpuccin (refer to repo if needed)
+# TODO lookup nixos wiki on bluetooth (blueman)
+# TODO flatpak support and flatseal
+# TODO scanning, printing, firewall
+# TODO mpd - music player daemon
+# TODO minecraft
+# TODO nvidia stuff
+# TODO rog stuff - batteries, etc
+# TODO install hypr stuff - cursor, graphics, idle, land, lang, lock, paper, polkitagent, utils, scanner, portal
+# TODO is xvideo wayland bridge required?
+# TODO pavucontrol
+# TODO hwinfo/lstopo
+# TODO meld
+# TODO networkmanager ?
+# TODO geo handler for gammastep
+# TODO xwayland ?
+# TODO librewolf ungoogled-chromium ?
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "kierkegaard";
+  home.homeDirectory = "/home/kierkegaard";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # command line utils
+    pkgs.oh-my-posh
+    pkgs.eza
+    pkgs.zoxide
+    pkgs.fzf
+    pkgs.jq
+    pkgs.fd
+    pkgs.ripgrep
+    pkgs.newsraft
+    pkgs.vim
+
+    # command line applications
+    pkgs.yazi
+    pkgs.btop
+
+    # programming software
+    pkgs.neovim
+    pkgs.minizinc
+    pkgs.minizincide # check if a desktop file is also provided
+
+    # application software and utils
+    pkgs.vimiv-qt
+    pkgs.keepassxc
+    pkgs.flowtime
+    pkgs.zotero
+    pkgs.gammastep
+    pkgs.lxqt.pcmanfm-qt
+    pkgs.protonvpn-gui
+    pkgs.copyq
+    pkgs.lxqt.lxqt-archiver
+    pkgs.grim
+    pkgs.flameshot
+    pkgs.gimp
+    pkgs.kdePackages.kdenlive # TODO fix opengl thing
+    pkgs.libreoffice-qt-fresh
+    pkgs.kdiskmark
+    pkgs.zathura
+    pkgs.mousam
+    pkgs.mpv
+    pkgs.vlc
+    pkgs.localsend # TODO fix opengly thing
+    pkgs.obs-studio # TODO failed to initialise
+    pkgs.qbittorrent
+    pkgs.qutebrowser # TODO fix, EGL not available
+    pkgs.shortwave
+    pkgs.tenacity # TODO maybe some ffmpeg issue?
+    pkgs.wezterm # TODO issue with wayland / EGL?
+    # pkgs.kdePackages.kdeconnect-kde # TODO figure out how to set this
+    # pkgs.kitty # TODO figure out how to set this and see if this needs to be set somewhere else
+    # pkgs.gsmartcontrol # facing some issues with polkit agent, need to fix
+    # pkgs.gparted # facing some issues with polkit agent, need to fix
+    # TODO might need to add xorg-xhost for the above stuff or look for options
+
+    # messengers
+    pkgs.signal-desktop
+    pkgs.element-desktop
+    pkgs.discord
+    pkgs.slack
+
+    # music
+    pkgs.spotube # TODO opengl stuff
+    # pkgs.kdePackages.audiotube # not working
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/kierkegaard/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # managing fonts
+  fonts.fontconfig.enable = true;
+}
