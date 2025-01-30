c1fec27c51e54a45fe674772a8e2ac9aeb6e280f
Joel Joseph Reji <joeljosephreji@gmail.com>
2025-01-30 22:42:09 +0530

    added pipewire support

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -178,4 +178,15 @@
     '';
   };
 
+  # pipewire stuff
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
 }
