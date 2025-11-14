00e3d3ada74e9808237eda4d66cb0936f5ddbcea
Joel Joseph Reji <joeljosephreji@gmail.com>
2025-11-14 18:47:19 +1000

    removing tenacity till it is fixed

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -99,7 +99,7 @@
     # pkgs.obs-studio # TODO: check if flatpak is a better option
     pkgs.qbittorrent
     pkgs.shortwave
-    pkgs.tenacity
+    # pkgs.tenacity # TODO: keep an eye till it is fixed
     pkgs.wezterm
     pkgs.calibre
     pkgs.pwvucontrol
